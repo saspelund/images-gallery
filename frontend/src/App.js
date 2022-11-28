9b87fc2395140d442fa4c2f3708bf685a0ecaeb7
Sanders Aspelund <aspelun1@msu.edu>
2022-11-27 19:05:23 -0500

    initial Header component2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+
 const App = () => {
   return (
     <div>
-      <h1>Images Gallery</h1>
+      {/* <h1>Images Gallery</h1> */}
+      {/* <Header></Header> */}
+      < Header title="Images Gallery"/>
     </div>
   );
 }
